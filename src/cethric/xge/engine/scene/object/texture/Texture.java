3db8bd5112226c78abbbdb4092ed0e3296c16b3a
Cethric <roganBlake@gmail.com>
2015-03-21 11:39:20 +1100

    New texture system, not currently implemented into the main game engine but the concept is there

--- src/cethric/xge/engine/scene/object/texture/Texture.java
+++ src/cethric/xge/engine/scene/object/texture/Texture.java
@@ -1,7 +1,43 @@
 package cethric.xge.engine.scene.object.texture;
 
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+
+import java.io.File;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+
 /**
  * Created by blakerogan on 19/03/15.
  */
 public class Texture {
+    private static int textureCount = 0;
+    private String textureName = String.format("Texture.%03d", textureCount);
+
+    private int textureTarget = GL11.GL_TEXTURE_2D;
+    private int textureID;
+    private int activeTexture = GL13.GL_TEXTURE0;
+
+    public Texture(File source) {
+        textureCount++;
+        textureName = source.getName();
+        init();
+    }
+
+    public void init() {
+        textureID = glGenTextures();
+    }
+
+    public void bind() {
+        glEnable(textureTarget);
+        glActiveTexture(activeTexture);
+        glBindTexture(textureTarget, textureID);
+    }
+
+    public void unbind() {
+        glBindTexture(textureTarget, 0);
+        glDisable(textureTarget);
+    }
+
 }
