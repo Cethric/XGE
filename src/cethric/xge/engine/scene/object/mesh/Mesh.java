00fb99308029944882ab7a55a13ec3b87f635215
Cethric <roganBlake@gmail.com>
2015-03-21 11:37:43 +1100

    Added new mesh rendering system

---
+++ src/cethric/xge/engine/scene/object/mesh/Mesh.java
@@ -0,0 +1,66 @@
+package cethric.xge.engine.scene.object.mesh;
+
+import cethric.xge.engine.scene.shader.ShaderProgram;
+import com.hackoeur.jglm.Mat4;
+
+/**
+ * Created by blakerogan on 21/03/15.
+ */
+public abstract class Mesh implements IMesh {
+    static int meshCount = 0;
+    private String meshName = String.format("Mesh.%03d", meshCount);
+
+    public Mesh() {
+        meshCount++;
+    }
+
+    /**
+     * Get the name of the mesh
+     *
+     * @return String; the name of the mesh
+     */
+    @Override
+    public String getName() {
+        return meshName;
+    }
+
+    /**
+     * Set the name of the mesh
+     *
+     * @param name String; the new name for the mesh
+     */
+    @Override
+    public void setName(String name) {
+        this.meshName = name;
+    }
+
+    /**
+     * Called on every update tick
+     *
+     * @param delta long; time since last update
+     */
+    @Override
+    public abstract void update(long delta);
+
+    /**
+     * Called every time the frame needs to be rendered.
+     *
+     * @param V             Mat4; Projection * View matrix. times the model matrix to get the MVP matrix.
+     * @param P             Mat4; Projection * View matrix. times the model matrix to get the MVP matrix.
+     * @param shaderProgram ShaderProgram; the attached shader program that renders this cube.
+     */
+    @Override
+    public abstract void render(Mat4 V, Mat4 P, ShaderProgram shaderProgram);
+
+    /**
+     * Setup the OpenGL and Bullet content as well as any other content that needs to be setup after creation
+     */
+    @Override
+    public abstract void setup();
+
+    /**
+     * Remove any content that this object no longer needs as it will be deleted.
+     */
+    @Override
+    public abstract void teardown();
+}
